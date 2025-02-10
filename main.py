21bd5f540595a417b20862942210c6bb8907ad34
notaspect372 <notaspect372@gmail.com>
2025-02-10 19:09:12 +0500

    Update main.py

--- main.py
+++ main.py
@@ -152,6 +152,6 @@ def scrape_multiple_urls(urls, start_page, end_page):
 
 # Example usage
 urls = [
-    "https://bj.coinafrique.com/search?sort_by=last&category=14&price_min=1000000&price_max=10000000&page=1"
+    "https://bj.coinafrique.com/search?sort_by=last&category=14&price_min=10000000&price_max=15000000&page=1"
 ]
 scrape_multiple_urls(urls, start_page=1, end_page=79)
